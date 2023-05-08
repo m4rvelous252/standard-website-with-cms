251bd3fdc8f814053bee28fd404f1a3c717e32af
Truong-Nghia <truongnghia25298@gmail.com>
2023-05-08 22:09:07 +0700

    modify tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,17 @@
-import { type Config } from "tailwindcss";
+import { type Config } from 'tailwindcss';
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0de6a7',
+        secondary: '#e60d4c',
+      },
+      gridTemplateColumns: {
+        fluid: 'repeat(auto-fit, minmax(15rem, 1fr))',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
