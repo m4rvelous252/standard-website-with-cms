6314aa4d29b67a2c84755429740db74861fb8792
Truong-Nghia <truongnghia25298@gmail.com>
2023-05-08 20:49:32 +0700

    add prettier config and clear head

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
 };
 
 module.exports = config;
