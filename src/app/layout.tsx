1f7bc5a870cf8c9bfe62a1e1f5f1e9950b30e6d5
Truong-Nghia <truongnghia25298@gmail.com>
2023-05-14 11:45:44 +0700

    rebase to get slices

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import Footer from "~/components/layout/Footer";
+import Navbar from "~/components/layout/Navbar";
+import '../styles/globals.css'
+
+export const metadata = {
+  title: 'Website Name',
+  description: 'Made with create-t3-app',
+  content: 'Landing page'
+};
+
+export default function RootLayout({ children }: {
+  children: React.ReactNode,
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+function Home() {
+  return (
+    <div>Landing Page</div>
+  )
+}
+
+export default Home
