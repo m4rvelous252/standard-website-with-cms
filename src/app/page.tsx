95d08ee3d26125de74e6393a40e3b85ce361407e
Truong-Nghia <truongnghia25298@gmail.com>
2023-05-21 18:30:09 +0700

    Add react icons and services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import ClientHighlight from "~/components/pages/home/ClientHighlight"
 import HeroSection from "~/components/pages/home/HeroSection"
 import FeatureSection from "~/components/pages/home/FeatureSection"
 import * as prismicH from '@prismicio/helpers'
+import ServiceSection from "~/components/pages/home/ServiceSection"
 
 const fetchHomePageData = async () => {
   const client = createClient()
@@ -41,6 +42,50 @@ const FeatureSectionProps = {
   imgUrl: 'assets/images/growth-analytics.svg'
 }
 
+
+const ServiceSectionProps = {
+  heading: 'Our Services',
+  subHeading: 'We craft digital, graphic and dimensional thinking, to create category leading brand experiences that have meaning and add a value for our clients.',
+  services: [
+    {
+      name: 'Digital Design',
+      description: "Some quick example text to build on the card title and make up the bulk of the card's content. Moltin gives you the platform.",
+    },
+    {
+      name: 'Unlimited Colors',
+      description: "Credibly brand standards compliant users without extensible services. Anibh euismod tincidunt ut laoreet Ipsum passage.",
+    },
+    {
+      name: 'Strategy Solutions',
+      description: "Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean necessary regelialia.",
+    },
+    {
+      name: 'Awesome Support',
+      description: "It is a paradisematic country, in which roasted parts of sentences fly into your mouth leave for the far World.",
+    },
+    {
+      name: 'Truly Multipurpose',
+      description: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.",
+    },
+    {
+      name: 'Easy to customize',
+      description: "Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia.",
+    },
+    {
+      name: 'Pixel Perfect Design',
+      description: "Some quick example text to build on the card title and make up the bulk of the card's content. Moltin gives you the platform.",
+    },
+    {
+      name: 'Perfect Toolbox',
+      description: "Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur.",
+    },
+    {
+      name: 'Awesome Design',
+      description: "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+    },
+  ]
+}
+
 async function Home() {
   const { HeroSectionProps, ClientHighlightProps } = await fetchHomePageData()
 
@@ -50,6 +95,7 @@ async function Home() {
         <HeroSection {...HeroSectionProps} />
         <ClientHighlight {...ClientHighlightProps} />
         <FeatureSection {...FeatureSectionProps} />
+        <ServiceSection {...ServiceSectionProps} />
       </>
     )
   } else {
---
+++ src/components/pages/home/ServiceSection.tsx
@@ -0,0 +1,58 @@
+import {
+  ImDisplay,
+  GiCutDiamond,
+  BsPiggyBank,
+  GiMaterialsScience,
+  IoNewspaperOutline,
+  SlPlane,
+  GoTools,
+  RxTimer,
+  HiCubeTransparent
+} from 'react-icons/all'
+import PageWidth from '~/components/layout/PageWidth'
+
+interface ServiceProps {
+  heading: string
+  subHeading: string
+  services: {
+    name: string
+    description: string
+  }[]
+}
+
+const icons = [
+  <GiCutDiamond className='h-8 basis-8 grow-0 shrink-0 text-primary' />,
+  <ImDisplay className='h-8 basis-8 grow-0 shrink-0 text-primary' />,
+  <BsPiggyBank className='h-8 basis-8 grow-0 shrink-0 text-primary' />,
+  <GiMaterialsScience className='h-8 basis-8 grow-0 shrink-0 text-primary' />,
+  <IoNewspaperOutline className='h-8 basis-8 grow-0 shrink-0 text-primary' />,
+  <SlPlane className='h-8 basis-8 grow-0 shrink-0 text-primary' />,
+  <GoTools className='h-8 basis-8 grow-0 shrink-0 text-primary' />,
+  <RxTimer className='h-8 basis-8 grow-0 shrink-0 text-primary' />,
+  <HiCubeTransparent className='h-8 basis-8 grow-0 shrink-0 text-primary' />
+]
+
+const ServiceSection = ({ services, heading, subHeading }: ServiceProps) => {
+  return (
+    <section className='bg-gray-50 py-16'>
+      <PageWidth className='flex flex-col items-center justify-center gap-10'>
+        <h1 className='font-bold text-3xl'>{heading}</h1>
+        <div className="w-[100px] h-1 bg-primary"></div>
+        <h4 className='text-gray-400'>{subHeading}</h4>
+        <ul className='grid grid-cols-1 gap-8 lg:grid-cols-3 lg:gap-12'>
+          {services.map(({ name, description }, index) => (
+            <li className='flex items-start'>
+              {icons[index]}
+              <div className="flex flex-col ml-6">
+                <span>{name}</span>
+                <span className='text-gray-400'>{description}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </PageWidth>
+    </section>
+  )
+}
+
+export default ServiceSection
